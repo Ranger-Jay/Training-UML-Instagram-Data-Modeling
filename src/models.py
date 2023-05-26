d00b3fec17195495d960a1f27833fcc56b8b9f25
Ranger-Jay <careerforjay@gmail.com>
2023-05-26 01:18:52 +0000

    final commit

--- src/models.py
+++ src/models.py
@@ -7,13 +7,13 @@ from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
-
+##ok 
 class Follower(Base):
     __tablename__ = 'follower'
     id = Column(Integer,primary_key=True)
     user_from_id = Column(Integer, ForeignKey('user.id'))
     user_to_id = Column(Integer, ForeignKey('user.id'))
-
+##ok 
 class Comment(Base): 
     __tablename__ = 'comment'
     id = Column(Integer , primary_key=True)
@@ -24,21 +24,21 @@ class Comment(Base):
     def to_dict(self):
         return {}
 
-
+##ok 
 class Media(Base):
     __tablename__ = 'media'
     id = Column(Integer , primary_key=True)
     media_type = Column(Integer) 
     url = Column(String(250)) ##ok 
     post_id = Column(Integer, ForeignKey('post.id'))
-
+##ok 
 class Post(Base):
     __tablename__ = 'post'
     id = Column(Integer , primary_key=True)
     user_id = Column(Integer , ForeignKey('user.id'))
     media = relationship(Media)
     comment = relationship(Comment)
-
+##ok 
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
@@ -50,11 +50,11 @@ class User(Base):
     comment = relationship(Comment)
     post = relationship(Post)
 
-
+##ok 
 ## Draw from SQLAlchemy base. finished.
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
-except Exception as e:
+    except Exception as e:
     print("There was a problem genering the diagram")
     raise e
